6a4c8b6266284d1529050ae1ff16c7bfbfbe067e
isuru <isuru.herath@netstarter.com>
2019-01-17 22:09:55 +0530

    Browserify added

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     concat = require('gulp-concat'),
+    browserify = require('gulp-browserify')
     compass = require('gulp-compass');
 
 gulp.task('log', function () {
@@ -28,6 +29,7 @@ gulp.task('coffee', function () {
 gulp.task('js', function(){
     gulp.src(jsSources)
     .pipe(concat('script.js'))
+    .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'))
 })
 
